d8385927209861e28920f2299e5be800fb13597a
Vanmathi S <VanmathiS@hexaware.com>
2018-09-17 18:07:24 +0530

    change in employeemapper.java file

--- restservice/leavemanager/src/main/java/com/hexaware/ftp82/persistence/EmployeeMapper.java
+++ restservice/leavemanager/src/main/java/com/hexaware/ftp82/persistence/EmployeeMapper.java
@@ -22,6 +22,8 @@ public class EmployeeMapper implements ResultSetMapper<Employee> {
     /**
      * @return Employee
      */
-    return new Employee(rs.getInt("EMP_ID"));
+    return new Employee(rs.getInt("EMP_ID"), rs.getString("EMP_NAME"), rs.getInt("EMP_PH"),
+       rs.getString("EMP_EMAIL"), rs.getString("EMP_DEPT"), rs.getInt("EMP_MANAGER_ID"),
+       rs.getInt("EMP_LEAVE_BALANCE"), rs.getDate("EMP_DOJ"));
   }
 }
