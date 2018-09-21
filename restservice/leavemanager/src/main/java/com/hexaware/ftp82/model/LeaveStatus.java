5bb834179fdfa8b2e6f947264d98f749f448618b
Vanmathi S <VanmathiS@hexaware.com>
2018-09-21 15:33:11 +0530

    added enum of leavestatus and leavetype

---
+++ restservice/leavemanager/src/main/java/com/hexaware/ftp82/model/LeaveStatus.java
@@ -0,0 +1,10 @@
+/**
+ * Employee class to store employee personal details.
+ * @author hexware
+ */
+enum LeaveStatus {
+/**
+ *@param decalres PENDING, APPROVED, DENIED as leave status
+ */
+    PENDING, APPROVED, DENIED;
+}
---
+++ restservice/leavemanager/src/main/java/com/hexaware/ftp82/model/LeaveType.java
@@ -0,0 +1,10 @@
+/**
+ * Employee class to store employee personal details.
+ * @author hexware
+ */
+enum LeaveType {
+/**
+ *@param decalres EL as leave type
+ */
+    EL;
+}
