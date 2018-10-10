6367c897678fb70d10dd7cdd7e9308b7599bc8ce
vinothm-hexaware <vinothm@hexaware.com>
2018-10-10 17:08:48 +0530

    rest done-LD

--- restservice/leavemanager/src/main/java/com/hexaware/ftp82/util/LeaveDetailsRest.java
+++ restservice/leavemanager/src/main/java/com/hexaware/ftp82/util/LeaveDetailsRest.java
@@ -1,6 +1,7 @@
 package com.hexaware.ftp82.util;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -43,5 +44,25 @@ public class LeaveDetailsRest {
       throw new NotFoundException("No such Employee ID: " + id);
     }
     return ls1;
+  }
+  /**
+   * Returns a specific employee's details.
+   * @param iD of the employee
+   * @param leaveType of the employee
+   * @param startDate of the employee
+   * @param endDate of the employee
+   * @param leaveReason of the employee
+   * @return the employee details
+   */
+  @POST
+  @Path("/applyleave/{iD}/{leaveType}/{startDate}/{endDate}/{leaveReason}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public final String applyLeaveRest(@PathParam("iD") final int iD, @PathParam("leaveType") final String leaveType, @PathParam("startDate") final String startDate, @PathParam("endDate") final String endDate, @PathParam("leaveReason") final String leaveReason) {
+    int status = LeaveDetails.applyLeave(iD, leaveType, startDate, endDate, leaveReason);
+    if (status > 0) {
+      return "inserted sucessfully";
+    } else {
+      return "failed during insertion try again";
+    }
   }
 }
