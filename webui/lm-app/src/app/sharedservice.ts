99deece6cb4b8e01ee473dda0d8bb53fc66ab008
AjithaC <AjithaC96>
2018-10-26 12:32:01 +0530

    Shared services

--- webui/lm-app/src/app/sharedservice.ts
+++ webui/lm-app/src/app/sharedservice.ts
@@ -9,46 +9,4 @@ export class SharedService {
     getId():number{
         return this.empId;
     }
-    empName:String;
-    setName(empName:String):void{
-        this.empName=empName;
-    }
-    getName():String{
-        return this.empName;
-    }
-    empPh:number;
-    setPh(empPh:number):void{
-        this.empPh=empPh;
-    }
-    getPh():number{
-        return this.empPh;
-    }
-    empEmail:String;
-    setEmail(empEmail:String):void{
-        this.empEmail=empEmail;
-    }
-    getEmail():String{
-        return this.empEmail;
-    }
-    empDept:String;
-    setDept(empDept:String):void{
-        this.empDept=empDept;
-    }
-    getDept():String{
-        return this.empDept;
-    }
-    empManagerId:String;
-    setManagerId(empManagerId:String):void{
-        this.empManagerId=empManagerId;
-    }
-    getManagerId():String{
-        return this.empManagerId;
-    }
-    empDoj:Date;
-    setDoj(empDoj:Date):void{
-        this.empDoj=empDoj;
-    }
-    getDoj():Date{
-        return this.empDoj;
-    }
 }
